75e4be4035c918af9b924105bab5916cb8bd366a
Akash <akashikur011@gmail.com>
2024-09-03 18:03:54 +0530

    first commit

--- src/app/(customAuth)/signin/page.tsx
+++ src/app/(customAuth)/signin/page.tsx
@@ -21,7 +21,6 @@ const Signin = () => {
     });
     if (result?.ok) {
       setLoading((prev) => !prev);
-      router.refresh();
       router.push("/dashboard");
     } else {
       setLoading((prev) => !prev);
